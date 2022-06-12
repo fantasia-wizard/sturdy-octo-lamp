bddb9c3f17ed58dc74e8eb2f800557f1be5966cc
Sergei <nstandfest@kmail.org>
2022-06-11 20:44:10 -0500

    Moved script from home index.htmdex.html to script.js.

---
+++ home/script.js
@@ -0,0 +1,17 @@
+var games = ["Fantasia", "steve", "Fantasia", "Fantasia", "Fantasia", "fantasia"];
+for (var i in games){
+  const app = document.createElement("div");
+  const new_app = document.createElement("a");
+  const new_app_image = document.createElement("img");
+  const new_app_title = document.createElement("p");
+  document.getElementById("cat").appendChild(app);
+  app.appendChild(new_app);
+  app.setAttribute("class", "app");
+  new_app.href = "../games/" + games[i];
+  new_app.appendChild(new_app_image);
+  new_app_image.src = "../games/" + games[i] + "/media/icon.png";
+  new_app_image.style.width = "100%";
+  new_app_image.alt = "Icon for " + games[i];
+  new_app.appendChild(new_app_title);
+  new_app_title.innerHTML = games[i];
+}
